65761664536c5ac6bc809f450faff7a4dd5bc27f
goodsun <goodsun0317@gmail.com>
2025-08-20 23:39:22 +0900

    onchain svgs

--- projects/effectBank/contracts/effectBank.sol
+++ projects/effectBank/contracts/effectBank.sol
@@ -1,41 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-interface IBackgroundBank {
-    function getBackgroundSVG(uint8 BackgroundId) external pure returns (string memory);
-    function getBackgroundName(uint8 BackgroundId) external pure returns (string memory);
+interface IEffectBank {
+    function getEffectSVG(uint8 EffectId) external pure returns (string memory);
+    function getEffectName(uint8 EffectId) external pure returns (string memory);
 }
 
-contract BackgroundBank {
-    IBackgroundBank public bank1;
-    IBackgroundBank public bank2;
-    IBackgroundBank public bank3;
+contract EffectBank {
+    IEffectBank public bank1;
+    IEffectBank public bank2;
+    IEffectBank public bank3;
 
     constructor(address _bank1, address _bank2, address _bank3) {
-        bank1 = IBackgroundBank(_bank1);
-        bank2 = IBackgroundBank(_bank2);
-        bank3 = IBackgroundBank(_bank3);
+        bank1 = IEffectBank(_bank1);
+        bank2 = IEffectBank(_bank2);
+        bank3 = IEffectBank(_bank3);
     }
 
-    function getBackgroundSVG(uint8 BackgroundId) public view returns (string memory) {
-        if (BackgroundId < 4) {
-            return bank1.getBackgroundSVG(BackgroundId);
-        } else if (BackgroundId < 8) {
-            return bank2.getBackgroundSVG(BackgroundId);
-        } else if (BackgroundId < 12) {
-            return bank3.getBackgroundSVG(BackgroundId);
+    function getEffectSVG(uint8 EffectId) public view returns (string memory) {
+        if (EffectId < 4) {
+            return bank1.getEffectSVG(EffectId);
+        } else if (EffectId < 8) {
+            return bank2.getEffectSVG(EffectId);
+        } else if (EffectId < 12) {
+            return bank3.getEffectSVG(EffectId);
         }
-        revert("Invalid Background ID");
+        revert("Invalid Effect ID");
     }
 
-    function getBackgroundName(uint8 BackgroundId) public view returns (string memory) {
-        if (BackgroundId < 4) {
-            return bank1.getBackgroundName(BackgroundId);
-        } else if (BackgroundId < 18) {
-            return bank2.getBackgroundName(BackgroundId);
-        } else if (BackgroundId < 12) {
-            return bank3.getBackgroundName(BackgroundId);
+    function getEffectName(uint8 EffectId) public view returns (string memory) {
+        if (EffectId < 4) {
+            return bank1.getEffectName(EffectId);
+        } else if (EffectId < 18) {
+            return bank2.getEffectName(EffectId);
+        } else if (EffectId < 12) {
+            return bank3.getEffectName(EffectId);
         }
-        revert("Invalid Background ID");
+        revert("Invalid Effect ID");
     }
 }
